98301d0fe17bb9a6597abf3412f401bacd3f8874
hanwldn76 <hanwldn76@gmail.com>
2024-10-20 20:12:45 +0900

    init

---
+++ src/main/java/com/dangdang/tb_mong/TbMongApplication.java
@@ -0,0 +1,13 @@
+package com.dangdang.tb_mong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbMongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbMongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dangdang/tb_mong/TbMongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangdang.tb_mong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbMongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

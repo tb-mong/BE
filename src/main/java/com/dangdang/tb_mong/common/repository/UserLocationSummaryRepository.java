753ed69dadd308e06931a2b85da55c7eec9ae6b3
hanwldn76 <hanwldn76@gmail.com>
2024-11-04 20:29:21 +0900

    feature: 동네별 1위 사용자 조회 수정

--- src/main/java/com/dangdang/tb_mong/common/repository/UserLocationSummaryRepository.java
+++ src/main/java/com/dangdang/tb_mong/common/repository/UserLocationSummaryRepository.java
@@ -3,5 +3,9 @@ package com.dangdang.tb_mong.common.repository;
 import com.dangdang.tb_mong.common.entity.UserLocationSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserLocationSummaryRepository extends JpaRepository<UserLocationSummary, Long> {
+
+    List<UserLocationSummary> findAllByLocationId(Long locationId);
 }
---
+++ src/main/java/com/dangdang/tb_mong/dongne/dto/TopUserResponse.java
@@ -0,0 +1,11 @@
+package com.dangdang.tb_mong.dongne.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TopUserResponse {
+    String kmTopUser;
+    String countTopUser;
+}
